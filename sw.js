4e499ea67b11b694d68ab1da71c6abcef4b329ab
pajogusi <pajogusi.uk@gmail.com>
2026-08-27 20:30:36 +0100

    Force immediate PWA updates on iPhone

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-const CACHE = 'benfica-match-center-v18-live-score-13';
-const ASSETS = ["./", "./index.html", "./benfica.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/benfica-crest.svg", "./icons/clubs/academico-de-viseu.svg", "./icons/clubs/agf-aarhus.svg", "./icons/clubs/alverca.svg", "./icons/clubs/arouca.svg", "./icons/clubs/casa-pia.svg", "./icons/clubs/estoril.svg", "./icons/clubs/estrela-amadora.svg", "./icons/clubs/famalicao.svg", "./icons/clubs/fc-porto.svg", "./icons/clubs/gil-vicente.svg", "./icons/clubs/heart-of-midlothian.svg", "./icons/clubs/maritimo.svg", "./icons/clubs/moreirense.svg", "./icons/clubs/nacional.svg", "./icons/clubs/rio-ave.svg", "./icons/clubs/santa-clara.svg", "./icons/clubs/sc-braga.svg", "./icons/clubs/sl-benfica.svg", "./icons/clubs/sporting-cp.svg", "./icons/clubs/st-gallen.svg", "./icons/clubs/vitoria-sc.svg", "./icons/competitions/liga-portugal.svg", "./icons/competitions/taca-portugal.svg", "./icons/competitions/taca-liga.png", "./icons/competitions/europa-league.svg", "./icons/competitions/champions-league.svg", "./icons/competitions/conference-league.svg", "./icons/competitions/supertaca-portugal.svg", "./icons/competitions/uefa-supercup.svg"];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+const CACHE = 'benfica-match-center-v19-ios-refresh';
+const ASSETS = ["./", "./index.html", "./benfica.html", "./styles.css", "./app.js?v=19-ios-refresh", "./manifest.webmanifest", "./icons/benfica-crest.svg", "./icons/clubs/academico-de-viseu.svg", "./icons/clubs/agf-aarhus.svg", "./icons/clubs/alverca.svg", "./icons/clubs/arouca.svg", "./icons/clubs/casa-pia.svg", "./icons/clubs/estoril.svg", "./icons/clubs/estrela-amadora.svg", "./icons/clubs/famalicao.svg", "./icons/clubs/fc-porto.svg", "./icons/clubs/gil-vicente.svg", "./icons/clubs/heart-of-midlothian.svg", "./icons/clubs/maritimo.svg", "./icons/clubs/moreirense.svg", "./icons/clubs/nacional.svg", "./icons/clubs/rio-ave.svg", "./icons/clubs/santa-clara.svg", "./icons/clubs/sc-braga.svg", "./icons/clubs/sl-benfica.svg", "./icons/clubs/sporting-cp.svg", "./icons/clubs/st-gallen.svg", "./icons/clubs/vitoria-sc.svg", "./icons/competitions/liga-portugal.svg", "./icons/competitions/taca-portugal.svg", "./icons/competitions/taca-liga.png", "./icons/competitions/europa-league.svg", "./icons/competitions/champions-league.svg", "./icons/competitions/conference-league.svg", "./icons/competitions/supertaca-portugal.svg", "./icons/competitions/uefa-supercup.svg"];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => event.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
